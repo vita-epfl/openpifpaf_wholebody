c04d7be74037006344f259999abbde010721d94a
Duncan Zauss <Duncan.Zauss@gmx.net>
2021-02-16 14:17:50 +0100

    update setuptools

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='openpifpaf_wholebody',
     packages= ['openpifpaf_wholebody'],
-    version = '0.0.1',
+    version = '0.1.0',
     description='OpenPifPaf wholebody',
     author='Duncan Zauss',
-    author_email='duncan.zauss@gmx.net',
     url='https://github.com/vita-epfl/openpifpaf_wholebody',
 
     install_requires=[
